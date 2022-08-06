a9931a18d201851bbab42234fedb6ef18101fb49
Benjamin <benschuetze@gmx.de>
2022-08-06 16:32:17 +0200

    styles

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
 
+  steps:object[] = [
+    {
+      text: 'As a music producer I got into developing small plug-ins with tools like Max 4 Live.',
+      image: '../assets/img/walk-line.png'
+    },
+    {
+      text: 'Doing that I discovered my love for coding and developing.',
+      image: '../assets/img/heart-line.png'
+    },
+    {
+      text: 'I looked for the best opportunity to learn web development in Germany and found the Developer Akademie.',
+      image: '../assets/img/search-line.png'
+    },
+    {
+      text: 'I started with learning the basics of HTML, CSS and Javascript and got to developing a Jump and Run Game and single page web apps with Angular.',
+      image: '../assets/img/flight-takeoff-line.png'
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
